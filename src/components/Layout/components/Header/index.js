a4ea5db7f3e4713e885eb1fddd00e36b9dbd9e99
Helia ^^ <qkhai.trieu@gmail.com>
2022-06-10 15:40:52 +0700

    Khai push

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind';
+import styles from './Header.module.scss'
+  
+
+const cx = classNames.bind(styles) //bind cái object styles vào rồi trả ra method funcition
 function Header() {
-    return <h2> Header</h2>;
+    return <header className  ={cx('wrapper')}>
+        <div className ={cx('inner' )}> 
+        
+        </div>
+    </header>
 }
 
 export default Header;
